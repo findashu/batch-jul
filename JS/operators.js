9a98b42ae9b81c8c20c880482aec5b531e691f92
Ashu_Mishra <ashutosh@thruskills.com>
2019-07-04 08:59:31 +0530

    initail commit

---
+++ JS/operators.js
@@ -0,0 +1,27 @@
+// Assignment Operator;
+// Assignment =
+// Plus Assignment +=
+// Substract -=
+// Multiplication *=
+// Division /=
+// Modulus %=
+
+var a = 10;
+console.log(a);
+
+// Plus Assignment Operator;
+
+var foo = 10;
+
+// foo = foo + 10;
+
+foo += 10;
+
+console.log(foo);
+
+// Substract Assignment Operator;
+var prd = 50;
+
+prd -= 30
+console.log(prd);
+
