b44dbb026aa117c12e9d2d0fcd00a845a7f63db7
Ashu_Mishra <ashutosh@thruskills.com>
2019-07-09 07:21:40 +0530

    added datatypes

---
+++ JS/data-types/boolean.js
@@ -0,0 +1,17 @@
+var isAdmin = true;
+
+var isSuperUser = false;
+
+
+console.log(isAdmin);
+
+console.log(typeof isAdmin);
+
+// Falsy Values
+
+// 1. NaN
+// 2. undefined
+// 3. ''
+// 4. 0
+// 5. null
+// 6. Boolean false
