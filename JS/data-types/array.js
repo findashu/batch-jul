3a9889994a793dc084549b338814c25b34c1b45b
Ashu_Mishra <ashutosh@thruskills.com>
2019-07-10 20:57:36 +0530

    data types and if statement

---
+++ JS/data-types/array.js
@@ -0,0 +1,48 @@
+// Array Literal 
+
+
+var scores = [];
+
+console.log(scores);
+
+console.log(typeof scores);
+
+
+// Math,Eng,Science,History
+var results = [50,40,60,65];
+
+
+// read
+
+console.log(results[3]);
+
+
+results[4] = 75;
+
+
+console.log(results);
+
+results[15] = 'passed';
+
+console.log(results);
+
+
+// length
+// length count starts from 1
+// index count starts frm 0
+console.log(results.length);
+
+// push()
+
+results.push(2016);
+
+console.log(results);
+
+
+// pop()
+
+results.pop();
+
+console.log(results);
+
+
