195f53520c3817c40b1851b5ddd889d0b6ab41bc
Ashu_Mishra <ashutosh@thruskills.com>
2019-08-01 09:13:17 +0530

    dom

---
+++ JS/Browser-ENV/dom/accessElement.js
@@ -0,0 +1,26 @@
+// getElementsByTagName();
+
+
+console.log(document.getElementsByTagName('p')[0]);
+
+// getElementsByClassName();
+
+console.log(document.getElementsByClassName('myclass'))
+
+
+// getElementById();
+
+console.log(document.getElementById('firstPara'));
+
+
+// querySelector();
+//  querySelectorAll()
+
+
+console.log(document.querySelector('p'));
+console.log(document.querySelectorAll('p'));
+
+
+console.log(document.querySelector('.myclass'));
+
+console.log(document.querySelector('#firstPara'))
